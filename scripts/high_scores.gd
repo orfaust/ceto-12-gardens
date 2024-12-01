b9ef5d7c038296d4fb36411a21170e608effe4c5
Carlo Veneziano <carlorfeo@gmail.com>
2024-12-01 16:08:08 +0100

    added score rules

--- scripts/high_scores.gd
+++ scripts/high_scores.gd
@@ -1,14 +1,14 @@
 extends CanvasLayer
 
-@onready var grid_container: GridContainer = %GridContainer
-@onready var alias_container: HBoxContainer = %AliasContainer
+@onready var scores_container: GridContainer = %ScoresContainer
+@onready var prompt_container: HBoxContainer = %PromptContainer
 @onready var alias_edit: LineEdit = %AliasEdit
 @onready var save_score_value: Label = %SaveScoreValue
 
 signal save_pressed(alias: String)
 
 func _ready() -> void:
-	alias_container.hide()
+	prompt_container.hide()
 	pass
 
 func add_item(player_name: String, score: int):
@@ -19,19 +19,19 @@ func add_item(player_name: String, score: int):
 	scoreLabel.text = str(score)
 	scoreLabel.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
 
-	grid_container.add_child(nameLabel)
-	grid_container.add_child(scoreLabel)
+	scores_container.add_child(nameLabel)
+	scores_container.add_child(scoreLabel)
 
 func empty():
-	for child in grid_container.get_children():
+	for child in scores_container.get_children():
 		child.queue_free()
 
 func show_prompt(score):
 	save_score_value.text = str(score)
-	alias_container.show()
+	prompt_container.show()
 
 func hide_prompt():
-	alias_container.hide()
+	prompt_container.hide()
 
 func _on_save_score_button_button_down() -> void:
 	save_pressed.emit(alias_edit.text)
